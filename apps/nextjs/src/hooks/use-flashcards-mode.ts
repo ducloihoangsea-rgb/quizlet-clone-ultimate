a4598879c0d0bbb4bd8b08176858ac00ea094828
ducloihoangsea-rgb <ducloihoangsea@gmail.com>
2026-08-07 22:45:50 +0700

    fix: use useQuery instead of useSuspenseQuery for getProgress to prevent client crash

--- apps/nextjs/src/hooks/use-flashcards-mode.ts
+++ apps/nextjs/src/hooks/use-flashcards-mode.ts
@@ -9,12 +9,12 @@ import { useFlashcardsModeReducer } from "./use-flashcards-mode-reducer";
 export function useFlashcardsMode(id: string) {
   const [{ flashcards: initialFlashcards }] =
     api.studySet.byId.useSuspenseQuery({ id });
-  const [studyProgress] = api.studyProgress.getProgress.useSuspenseQuery({ studySetId: id });
+  const { data: studyProgress } = api.studyProgress.getProgress.useQuery({ studySetId: id });
   const { mutate: updateStatus } = api.studyProgress.updateFlashcardStatus.useMutation();
   const { mutate: resetProgressMutation } = api.studyProgress.resetFlashcardProgress.useMutation();
 
   const [{ sorting, flashcards, index, starredOnly, hard, know, trackProgress, learningCount, knownCount, history, frontFace, textToSpeech }, dispatch] =
-    useFlashcardsModeReducer(initialFlashcards, studyProgress);
+    useFlashcardsModeReducer(initialFlashcards, studyProgress ?? []);
 
   const [cardRef, animateCard] = useAnimate();
   const [messageRef, animateMessage] = useAnimate();
